87f540711da0f9f673645f83de730efd437d453d
Cliffwn <cliffwn@gmail.com>
2021-03-07 16:53:34 -0500

    finished logic, it's playable in console in-browser. Done until we come back upon this later.

--- scripts.js
+++ scripts.js
@@ -21,17 +21,29 @@ function computerPlay() {
 
 //     return log;
 // }
+function game() {
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return "It's a tie!"
-    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return "You lose, paper beats rock."
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return "You win, rock beats scissors."
-    } else if (playerSelection === 'paper' && computerSelection === 'rock')
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection === computerSelection) {
+            console.log("It's a tie!");
+        } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+            console.log("You lose, paper beats rock.");
+        } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+            console.log("You win, rock beats scissors.")
+        } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+            console.log("You win, paper beats rock.")
+        } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+            console.log("You lose, scissors beats paper.")
+        } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+            console.log("You lose, rock beats scissors.")
+        } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+            console.log("You win, scissors beats paper.")
+        }
+    }
+
+    const playerSelection = prompt("Make your choice: ");
+    const computerSelection = computerPlay();
+
+    console.log(playRound(playerSelection, computerSelection))
+
 }
-
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-
